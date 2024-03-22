28273b01f149b10fc0a7b7e9d5c31a9dab6bb1a0
Ed Finkler <coj@funkatron.com>
2024-03-21 21:01:09 -0400

    Refactor update.sh script
    - do not write a dated file if the content is identical to the current file
    - improve readability and add/improve logging

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
-# outputs the currently playing song to a file in the data directory
+# This script retrieves the current song playing on Apple Music, and writes it to a file.
+# It is intended to be run as a cron job every minute, and will only write to the file if the song has changed.
+# The script also logs its output to a file in the _logs directory.
 
 # get the script's directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -9,24 +11,34 @@ LOGSDIR=$DIR/_logs
 mkdir -p $DATADIR
 mkdir -p $LOGSDIR
 
-# log any output to a file: _logs/<script-name>_<date-started>.log
-exec &> $DIR/_logs/update_$(date +"%Y-%m-%d").log
+# define the file paths for the current song
+CURRENT_DATETIME=$(date +"%Y%m%d_%H%M%S")
+CURRENT_DATE=$(date +"%Y-%m-%d")
+LOG_PREFIX="[$(basename $0) $CURRENT_DATETIME]"
 
-# copy the previous current_song.txt to current_song-<creation_datetime>.txt, if it exists
-[ -f $DATADIR/current_song.txt ] && cp $DATADIR/current_song.txt $DATADIR/current_song-$(date +"%Y%m%d_%H%M%S").txt
+CURRENT_SONG_FILE=$DATADIR/current_song.txt
+CURRENT_SONG_TMP_FILE=$DATADIR/current_song-tmp.txt
+CURRENT_SONG_DATED_FILE=$DATADIR/played_song-$CURRENT_DATETIME.txt
+
+# log any output to a file: _logs/<script-name>_<date-started>.log
+exec &> $LOGSDIR/update_$CURRENT_DATE.log
 
 # run the python script using the venv interpreter to get the current song, and write it to a tmp file
-$DIR/venv/bin/python $DIR/get-apple-music-now-playing.py > $DATADIR/current_song-tmp.txt
-
+$DIR/venv/bin/python $DIR/get-apple-music-now-playing.py > $CURRENT_SONG_TMP_FILE
 
 # if the tmp file is not empty, move it to current_song.txt
-if [ -s $DATADIR/current_song-tmp.txt ]; then
+if [ -s $CURRENT_SONG_TMP_FILE ]; then
     # if the contents of the tmp file are identical to the current_song.txt, just remove the tmp file.
-    if cmp -s $DATADIR/current_song-tmp.txt $DATADIR/current_song.txt; then
-        rm $DATADIR/current_song-tmp.txt
-        echo "No change in current song; skipping update."
+    if cmp -s $CURRENT_SONG_TMP_FILE $CURRENT_SONG_FILE; then
+        rm $CURRENT_SONG_TMP_FILE
+        # echo "$LOG_PREFIX No change in current song; skipping update."
     else
-        mv $DATADIR/current_song-tmp.txt $DATADIR/current_song.txt
-        echo "Updated current song."
+        # copy the previous current_song.txt to current_song-<creation_datetime>.txt, if it exists
+        [ -f $CURRENT_SONG_FILE ] && cp $CURRENT_SONG_FILE $CURRENT_SONG_DATED_FILE
+        # move the tmp file to current_song.txt
+        mv $CURRENT_SONG_TMP_FILE $CURRENT_SONG_FILE
+        # get the first two lines of the current song file, if they exist, and write them to the log -- but with all newlines replaced with spaces
+        CURRENT_SONG=$(head -n 2 $CURRENT_SONG_FILE | tr '\n' ' by ')
+        echo "$LOG_PREFIX Updated current song: $CURRENT_SONG"
     fi
 fi
